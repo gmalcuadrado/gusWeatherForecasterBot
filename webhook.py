3d34d00369f72d263cb2d7f7a6351f4a66b066a3
gustavo.mendezcasero@online.liverpool.ac.uk <mendez@unicc.org>
2019-06-23 18:25:28 +0200

    test

--- webhook.py
+++ webhook.py
@@ -33,7 +33,7 @@ def makeResponse(req):
     city = parameters.get("geo-city")
     date = parameters.get("date")
     r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=35918c9922e8cac62623e7a20694eecb')
-    print"printing response R to API Open Weather Map:", r
+    print"printing response R to API Open Weather Map: ", r
 
 
     json_object = r.json()
