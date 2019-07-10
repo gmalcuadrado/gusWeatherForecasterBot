8338815ebef30f9cc6c831b0a7871ac6bdf2a879
gustavo.mendezcasero@online.liverpool.ac.uk <mendez@unicc.org>
2019-07-10 23:39:50 +0200

    changing speech

--- webhook.py
+++ webhook.py
@@ -78,7 +78,7 @@ def makeWeatherResponse(req):
     # print (json_object) # Debugging
 
     weather=json_object['list']
-    for i in range(0,30): # It should be something like len(weather):, not working
+    for i in range(0,60): # It should be something like len(weather):, not working
         if dateTimeFormated in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
             print ('printing condition', condition) # For debugging
