b83d211eda50ad1db5aa6ca2a86b2ee941d30bd2
gustavo.mendezcasero@online.liverpool.ac.uk <mendez@unicc.org>
2019-07-10 23:53:31 +0200

    WORKING - BEFORE PANDAS

--- webhook.py
+++ webhook.py
@@ -1,6 +1,6 @@
 import json # Parses JSON into a Python dictionary or list. 
 import os
-import requests # 
+import requests 
 # import boto3 # Amazon Web Services (AWS) SDK for Python. When I try to import it, ModuleNotFoundError: No module named 'boto3' and Python app does not work
 
 # Flask is a lightweight Python framework for web applications that provides the basics for URL routing and page rendering. 
