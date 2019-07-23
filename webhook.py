d76e62576667fa5a4fe9497a60e51a566d9c5ab4
gustavo.mendezcasero@online.liverpool.ac.uk <mendez@unicc.org>
2019-07-24 01:46:40 +0200

    ts

--- webhook.py
+++ webhook.py
@@ -49,7 +49,7 @@ def makeWriteGsmResponse(req):
     # Obtaining parameters from Dialogflow request    
     result = req.get("queryResult")
     parameters = result.get("parameters")
-    leaveDayRequestStr = parameters.get("amount")
+    leaveDayRequestStr = parameters.get("number-integer")
     
     print('printing leaveDay =', leaveDayRequestStr) # For debugging
 
