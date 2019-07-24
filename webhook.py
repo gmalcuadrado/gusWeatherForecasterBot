ef1aae481ab751ac2dd252b13208122d185059e0
gustavo.mendezcasero@online.liverpool.ac.uk <mendez@unicc.org>
2019-07-24 11:51:14 +0200

    ts

--- webhook.py
+++ webhook.py
@@ -31,7 +31,7 @@ def webhook(): # method app.route decorators create
     if parameters.get("geo-city"):
         return make_response(jsonify(makeWeatherResponse(req))) # Debugging, return sample from https://www.pragnakalp.com/dialogflow-fulfillment-webhook-tutorial/
 
-    elif parameters.get("duration"):
+    elif parameters.get("leaveRequestKey" or "leaveRequestKey.original" or "duration" or "duration.original"):
         return make_response(jsonify(makeWriteGsmResponse(req)))
 
     else:
